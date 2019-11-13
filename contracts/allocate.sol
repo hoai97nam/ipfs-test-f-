91f09b4bdb57df3b620f6fbc8c5b429cbad55bae
hoai97nam <nhnam97@gmail.com>
2019-11-13 13:54:52 +0700

    update: storageData2 contain address of content

--- contracts/allocate.sol
+++ contracts/allocate.sol
@@ -6,6 +6,8 @@ contract allocate {
     function() payable external {}
     string[] storedData =["QmYantVWYe2itHH68CwL3cqXHM6EwGmxH75nugQiq2uAF9","QmYisfKRSxWx3PKsn7Yfwcy2Hj2EdmWFXLZRpgKziRbwpB","QmSREQmvNEqHjC7CuuWHakLHAdsPFFfecPk5Kv9PTiU2Qb"];
     string[] storedData1=["perfect","home","survivors"];
+    address[] storedData2=["0xEDc30Bb67af5C126AF1400991Fa6d2F4B4760B5C",
+"0xD267cb5E20050289a893F2a832Bd5D6DDBe08820","0x86dED0814431e6dBAA3C86b32094613f3Ed9F3A0"];
     mapping(address => uint) public count;
     function upVote(address x) public 
     {
@@ -23,9 +25,10 @@ contract allocate {
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-    function set(string memory x, string memory y) public {
+    function set(string memory x, string memory y, address z) public {
         storedData.push(x);
         storedData1.push(y);
+        storedData2.push(z);
     }
     function get() public view returns (string[] memory, string[] memory) {
         return (storedData, storedData1);
