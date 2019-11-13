ea12076cdc8f1c59fefc6a0a3b0f2b6d2c715b41
hoai97nam <nhnam97@gmail.com>
2019-11-13 14:00:59 +0700

    Fix: address array syntax

--- contracts/allocate.sol
+++ contracts/allocate.sol
@@ -6,8 +6,8 @@ contract allocate {
     function() payable external {}
     string[] storedData =["QmYantVWYe2itHH68CwL3cqXHM6EwGmxH75nugQiq2uAF9","QmYisfKRSxWx3PKsn7Yfwcy2Hj2EdmWFXLZRpgKziRbwpB","QmSREQmvNEqHjC7CuuWHakLHAdsPFFfecPk5Kv9PTiU2Qb"];
     string[] storedData1=["perfect","home","survivors"];
-    address[] storedData2=["0xEDc30Bb67af5C126AF1400991Fa6d2F4B4760B5C",
-"0xD267cb5E20050289a893F2a832Bd5D6DDBe08820","0x86dED0814431e6dBAA3C86b32094613f3Ed9F3A0"];
+    address[] storedData2=[0xEDc30Bb67af5C126AF1400991Fa6d2F4B4760B5C,
+0xD267cb5E20050289a893F2a832Bd5D6DDBe08820,0x86dED0814431e6dBAA3C86b32094613f3Ed9F3A0];
     mapping(address => uint) public count;
     function upVote(address x) public 
     {
